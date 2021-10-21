d060430fc6f4d213d5b79ad49b575a77b97c6746
Enrico Notaro <notaro.enrico@gmail.com>
2021-10-21 21:58:39 +0200

    make it better

--- scripts/server.js
+++ scripts/server.js
@@ -3,6 +3,7 @@ const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
 const path = require('path');
+var cors = require('cors');
 
 const app = express();
 app.use(helmet());
@@ -12,7 +13,27 @@ const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3001;
 const DIST_DIR = './dist';
 
-app.use(function (req, res, next) {
+app.use(cors())
+
+app.options('*', cors())
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Accept, Authorization, Content-Type, X-Requested-With, Range,append,delete,entries,foreach,get,has,keys,set,values',
+  exposedHeaders: 'Content-Length',
+  preflightContinue : true,
+  optionsSuccessStatus: 200
+}));
+
+app.get("/oauth2/userinfo", cors(), (req, res) => {
+  console.info("GET /simple-cors");
+  res.json({
+    text: "Simple CORS requests are working. [GET]"
+  });
+});
+
+/*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin",
     "*");
   res.header("Access-Control-Allow-Methods",
@@ -22,7 +43,7 @@ app.use(function (req, res, next) {
     "Accept, Authorization, Content-Type, X-Requested-With, Range,append,delete,entries,foreach,get,has,keys,set,values");
 	res.header("Content-Security-Policy","connect-src 'self' https://notaroenrico-dev-ed.my.salesforce.com/")
   next();
-});
+});*/
 
 app.use(express.static(DIST_DIR));
 

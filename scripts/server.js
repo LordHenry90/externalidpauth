5be6431408875d5aa25ac4ea494a972e927b537c
Enrico Notaro <notaro.enrico@gmail.com>
2021-10-21 22:06:54 +0200

    make it better

--- scripts/server.js
+++ scripts/server.js
@@ -33,6 +33,9 @@ app.get("/oauth2/userinfo", cors(), (req, res) => {
   });
 });
 app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	res.header("Content-Security-Policy","connect-src 'self' https://notaroenrico-dev-ed.my.salesforce.com/")
   next();
 });

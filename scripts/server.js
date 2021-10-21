5672ada532b40d840029e36b233f9a0fe3deec90
Enrico Notaro <notaro.enrico@gmail.com>
2021-10-21 20:50:30 +0200

    make it better

--- scripts/server.js
+++ scripts/server.js
@@ -14,7 +14,7 @@ const DIST_DIR = './dist';
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin",
-    req.get("Origin") || "*");
+    "https://externalidpauth.herokuapp.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");

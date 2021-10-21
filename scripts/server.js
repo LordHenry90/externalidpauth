c558c4e13e7e4237b4c7b99cbf8d38d245e2da7f
Enrico Notaro <notaro.enrico@gmail.com>
2021-10-21 19:49:32 +0200

    make it better

--- scripts/server.js
+++ scripts/server.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Expose-Headers", "Content-Length");
   res.header("Access-Control-Allow-Headers",
     "Accept, Authorization, Content-Type, X-Requested-With, Range");
-	res.header("Content-Security-Policy","default-src 'self'")
+	res.header("Content-Security-Policy","connect-src 'self' https://notaroenrico-dev-ed.my.salesforce.com/")
   next();
 });
 

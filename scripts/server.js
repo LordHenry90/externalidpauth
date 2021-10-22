22b381e9d378a71d8e1c424f9f9e33d01d3aa946
LordHenry90 <notaro.enrico@gmail.com>
2021-10-22 19:57:41 +0200

    adasd

--- scripts/server.js
+++ scripts/server.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const express = require('express');
 const path = require('path');
 const app = express();
-const dotenv = require('dotenv').config();
+const dotenv = require('../preload.js');
 app.use(helmet());
 app.use(compression());
 
--- src/modules/my/loginView/loginView.js
+++ src/modules/my/loginView/loginView.js
@@ -1,6 +1,6 @@
 import { LightningElement, track } from 'lwc';
 
-const dotenv = require('./preload.js');
+const dotenv = require('../preload.js');
 
 export default class LoginView extends LightningElement {
     @track userAgentFlowUrlAuth = process.env.AUTH_PROVIDER_URL;

f2f3385384e9b0d54cfed636618f71b1f0783d04
LordHenry90 <notaro.enrico@gmail.com>
2021-10-22 17:28:05 +0200

    we

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import { createElement } from 'lwc';
 import MyApp from 'my/app';
 import '@lwc/synthetic-shadow';
 
-const dotenv = require('dotenv').config();
-
 const indexApp = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(indexApp);

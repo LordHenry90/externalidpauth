ba3e617658b7b25c78b7b8724b8e17a2103e80c7
LordHenry90 <notaro.enrico@gmail.com>
2021-10-22 19:41:00 +0200

    eda

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createElement } from 'lwc';
 import MyApp from 'my/app';
 import '@lwc/synthetic-shadow';
-const dotenv = require('./preload');
 
 const indexApp = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query

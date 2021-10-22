08ad94e49d065df955814853d3bdbe6ef07b42ba
LordHenry90 <notaro.enrico@gmail.com>
2021-10-22 18:46:56 +0200

    Update loginView.js

--- src/modules/my/loginView/loginView.js
+++ src/modules/my/loginView/loginView.js
@@ -1,5 +1,5 @@
 import { LightningElement, track } from 'lwc';
-import { config } from 'config';
+import { config } from './default';
 
 export default class LoginView extends LightningElement {
     @track userAgentFlowUrlAuth = config.get('authProviderURL');

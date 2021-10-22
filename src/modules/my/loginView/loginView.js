cf7f01d3194affa36c27b2d032cc893081eb4045
LordHenry90 <notaro.enrico@gmail.com>
2021-10-22 19:00:38 +0200

    we

--- src/modules/my/loginView/loginView.js
+++ src/modules/my/loginView/loginView.js
@@ -1,6 +1,5 @@
 import { LightningElement, track } from 'lwc';
-import { config } from './default';
 
 export default class LoginView extends LightningElement {
-    @track userAgentFlowUrlAuth = config.get('authProviderURL');
+    @track userAgentFlowUrlAuth = process.env.AUTH_PROVIDER_URL;
 }

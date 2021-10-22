cf949a28842b8a274f7b3e41965510f59aac6503
LordHenry90 <notaro.enrico@gmail.com>
2021-10-22 18:26:21 +0200

    we

---
+++ config/default.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+export const client = {
+    authProviderURL: process.env.AUTH_PROVIDER_URL,
+    callbackURL: process.env.CALLBACK_URL,
+    nodeENV: process.env.NODE_ENV,
+    sdfcURL: process.env.SALESFORCE_URL
+}
--- src/modules/my/loginView/loginView.js
+++ src/modules/my/loginView/loginView.js
@@ -1,6 +1,7 @@
 import { LightningElement, track } from 'lwc';
+import { config } from 'config';
 
 export default class LoginView extends LightningElement {
-
+    console.log(config.get('sdfcURL'));
     @track userAgentFlowUrlAuth = process.env.AUTH_PROVIDER_URL;
 }
